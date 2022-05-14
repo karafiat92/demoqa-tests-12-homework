018c16295658c0c57235785f081d1c0e0cf633bb
Екатерина Антонова <karafiat@mail.ru>
2022-05-14 10:18:12 +0700

    wrote functions, added comments, reduce code line amount

--- src/test/java/link/i/PracticeFormTests.java
+++ src/test/java/link/i/PracticeFormTests.java
@@ -2,6 +2,7 @@ package link.i;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +24,39 @@ public class PracticeFormTests {
     int monthNumber = 1;
     int yearNumber = 1;
 
+    //Кликаем на элемент
+    void clickOnElement(String locator) {
+        $(locator).click();
+    }
+    //Селектор имеет состояние
+    void elementShouldBe(String locator, Condition condition) {
+        $(locator).shouldBe(condition);
+    }
+    //Селектор содержит состояние
+    void elementShouldHave(String locator, Condition condition) {
+        $(locator).shouldHave(condition);
+    }
+    //Задаём значение и проверяем, что оно внеслось
+    void setValueAndCheck(String locator, String value) {
+        $(locator).setValue(value).shouldHave(value(value));
+    }
+    //Получаем текст элемента и возвращаем
+    String getElementText(String locator) {
+        String elementText = $(locator).getText();
+        return elementText;
+    }
+    //Выбираем элемент из дропдауна и возвращаем текст в нём
+    String chooseDropdownElement (String dropdown, int value){
+        String elementText = $(dropdown).$("option", value).getText();
+        $(dropdown).$("option", value).click();
+        return elementText;
+    }
+    //Ищем значение в таблице и возвращаем элемент селенида для дальнейшей проверки
+        SelenideElement checkValueOnTable (String rowTitle){
+            SelenideElement elem = $("tbody").find(byTagAndText("td", rowTitle)).sibling(0);
+        return elem;
+    }
+
     @BeforeAll
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -34,36 +68,39 @@ public class PracticeFormTests {
     void fillFormsWithPositiveValues() {
         open("/automation-practice-form");
 
-        $("#firstName").setValue(firstname).shouldHave(value(firstname));
+        setValueAndCheck("#firstName", firstname);
 
-        $("#lastName").setValue(lastName).shouldHave(value(lastName));
+        setValueAndCheck("#lastName", lastName);
 
-        $("#userEmail").setValue(userEmail).shouldHave(value(userEmail));
+        setValueAndCheck("#userEmail", userEmail);
 
-        $("label[for=gender-radio-1]").click();
-        $("input[id=gender-radio-1]").shouldBe(Condition.checked);
-        $("label[for=gender-radio-2]").click();
-        $("input[id=gender-radio-2]").shouldBe(Condition.checked);
-        $("label[for=gender-radio-3]").click();
-        String gender = $("label[for=gender-radio-3]").getText();
-        $("input[id=gender-radio-3]").shouldBe(Condition.checked);
+        clickOnElement("label[for=gender-radio-1]");
+
+        elementShouldBe("input[id=gender-radio-1]", checked);
+
+        clickOnElement("label[for=gender-radio-2]");
+
+        elementShouldBe("input[id=gender-radio-2]", checked);
+
+        clickOnElement("label[for=gender-radio-3]");
+
+        String gender = getElementText("label[for=gender-radio-3]");
+
+        elementShouldBe("input[id=gender-radio-3]", checked);
 
         $("#userNumber").setValue(numberEnter).shouldHave(value(numberEntered));
 
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-container").shouldBe(visible);
+        clickOnElement("#dateOfBirthInput");
 
-        $("select[class=react-datepicker__month-select]").click();
-        String month = $("select[class=react-datepicker__month-select]")
-                .$("option", monthNumber).getText();
-        $("select[class=react-datepicker__month-select]")
-                .$("option", monthNumber).click();
+        elementShouldBe(".react-datepicker__month-container", visible);
 
-        $("select[class=react-datepicker__year-select]").click();
-        String year = $("select[class=react-datepicker__year-select]")
-                .$("option", yearNumber).getText();
-        $("select[class=react-datepicker__year-select]")
-                .$("option", yearNumber).click();
+        clickOnElement("select[class=react-datepicker__month-select]");
+
+        String month = chooseDropdownElement("select[class=react-datepicker__month-select]", monthNumber);
+
+        clickOnElement("select[class=react-datepicker__year-select]");
+
+        String year = chooseDropdownElement("select[class=react-datepicker__year-select]", yearNumber);
 
         String day = $(".react-datepicker__week").lastChild().getText();
         $(".react-datepicker__week").lastChild().click();
@@ -75,73 +112,64 @@ public class PracticeFormTests {
         );
         $(".react-datepicker__month-container").shouldNotBe(visible);
 
-        $("#subjectsInput").setValue(subject);
-        String chosenSubject = $("#react-select-2-option-0").getText();
-        $("#react-select-2-option-0").click();
+        setValueAndCheck("#subjectsInput", subject);
 
-        $("label[for=hobbies-checkbox-1]").click();
-        String hobby1 = $("label[for=hobbies-checkbox-1").getText();
-        $("input[id=hobbies-checkbox-1]").shouldBe(Condition.checked);
-        $("label[for=hobbies-checkbox-2]").click();
-        String hobby2 = $("label[for=hobbies-checkbox-2").getText();
-        $("input[id=hobbies-checkbox-2]").shouldBe(Condition.checked);
-        $("label[for=hobbies-checkbox-3]").click();
-        String hobby3 = $("label[for=hobbies-checkbox-3").getText();
-        $("input[id=hobbies-checkbox-3]").shouldBe(Condition.checked);
+        String chosenSubject = getElementText("#react-select-2-option-0");
+        clickOnElement("#react-select-2-option-0");
+
+        clickOnElement("label[for=hobbies-checkbox-1]");
+        String hobby1 = getElementText("label[for=hobbies-checkbox-1");
+        elementShouldBe("input[id=hobbies-checkbox-1]", checked);
+
+        clickOnElement("label[for=hobbies-checkbox-2]");
+        String hobby2 = getElementText("label[for=hobbies-checkbox-2");
+        elementShouldBe("input[id=hobbies-checkbox-2]", checked);
+
+        clickOnElement("label[for=hobbies-checkbox-3]");
+        String hobby3 = getElementText("label[for=hobbies-checkbox-3");
+        elementShouldBe("input[id=hobbies-checkbox-3]", checked);
 
         $("#uploadPicture").uploadFromClasspath(pictureName);
         $("#uploadPicture").shouldHave(value(pictureName));
 
-        $("#currentAddress").setValue(currentAddress).shouldHave(value(currentAddress));
+        setValueAndCheck("#currentAddress",currentAddress);
 
-        $("#state").click();
-        String state = $("#react-select-3-option-2").getText();
-        $("#react-select-3-option-2").click();
+        clickOnElement("#state");
+        String state = getElementText("#react-select-3-option-2");
+        clickOnElement("#react-select-3-option-2");
+        elementShouldHave(".css-1uccc91-singleValue", text(state));
+
+        clickOnElement("#city");
+        String city = getElementText("#react-select-4-option-0");
+        clickOnElement("#react-select-4-option-0");
         $(".css-1uccc91-singleValue").shouldHave(text(state));
 
-        $("#city").click();
-        String city = $("#react-select-4-option-0").getText();
-        $("#react-select-4-option-0").click();
-        $(".css-1uccc91-singleValue").shouldHave(text(state));
+        clickOnElement("#submit");
 
-        $("#submit").click();
-
+        //Проверяем, что все данные ушли правильно
         $(".modal-content").should(exist);
 
-        $("tbody").find("tr").shouldHave(text("Student Name"))
-                .lastChild()
-                .shouldHave(text(firstname), text(lastName));
-        $("tbody").find(byTagAndText("td", "Student Name"))
-                .sibling(0)
-                .shouldHave(text(firstname), text(lastName));
-        $("tbody").find(byTagAndText("td", "Student Email"))
-                .sibling(0)
-                .shouldHave(text(userEmail));
-        $("tbody").find(byTagAndText("td", "Gender"))
-                .sibling(0)
-                .shouldHave(text(gender));
-        $("tbody").find(byTagAndText("td", "Mobile"))
-                .sibling(0)
-                .shouldHave(text(numberEntered));
-        $("tbody").find(byTagAndText("td", "Date of Birth"))
-                .sibling(0)
-                .shouldHave(text(day), text(month), text(year));
-        $("tbody").find(byTagAndText("td", "Subjects"))
-                .sibling(0)
-                .shouldHave(text(chosenSubject));
-        $("tbody").find(byTagAndText("td", "Hobbies"))
-                .sibling(0)
-                .shouldHave(text(hobby1), text(hobby2), text(hobby3));
-        $("tbody").find(byTagAndText("td", "Picture"))
-                .sibling(0)
-                .shouldHave(text(pictureName));
-        $("tbody").find(byTagAndText("td", "Address"))
-                .sibling(0)
-                .shouldHave(text(currentAddress));
-        $("tbody").find(byTagAndText("td", "State and City"))
-                .sibling(0)
-                .shouldHave(text(state), text(city));
-        $("#closeLargeModal").click();
+        checkValueOnTable( "Student Name").shouldHave(text(firstname), text(lastName));
+
+        checkValueOnTable("Student Email").shouldHave(text(userEmail));
+
+        checkValueOnTable("Gender").shouldHave(text(gender));
+
+        checkValueOnTable("Mobile").shouldHave(text(numberEntered));
+
+        checkValueOnTable("Date of Birth").shouldHave(text(day), text(month), text(year));
+
+        checkValueOnTable("Subjects").shouldHave(text(chosenSubject));
+
+        checkValueOnTable("Hobbies").shouldHave(text(hobby1), text(hobby2), text(hobby3));
+
+        checkValueOnTable("Picture").shouldHave(text(pictureName));
+
+        checkValueOnTable("Address").shouldHave(text(currentAddress));
+
+        checkValueOnTable("State and City").shouldHave(text(state), text(city));
+
+        clickOnElement("#closeLargeModal");
         $(".modal-content").shouldNot(exist);
     }
 }
